a2fe476d266a475d50100dbb08e4a3c2545ba6f8
Wojciech Sobociński <sobota96@wp.pl>
2020-10-19 18:15:47 +0200

    Initial commit

---
+++ src/test/java/com/wsobocinski/footballclubsearcher/FootballClubSearcherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wsobocinski.footballclubsearcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballClubSearcherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
